7a49da55ba83aafcc896ecf1d243108645d0a6df
otw7917 <otw7917@gmail.com>
2022-12-04 20:37:16 +0900

    feature:show popular video list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import VideoList from "./components/videoList";
+
 function App() {
   return (
     <div className='App'>
-      <div className='text-xl font-bold'>
-        <h1>tailwind</h1>
-      </div>
+      <VideoList />
     </div>
   );
 }
