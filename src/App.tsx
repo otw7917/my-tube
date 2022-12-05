b403fe7b0ed964f2ade9cd0854a26a490162c3ed
otw7917 <otw7917@gmail.com>
2022-12-05 14:06:39 +0900

    비디오 조회 네트워크 요청 방지 주석처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from "./components/header";
 import VideoList from "./components/videoList";
 
 function App() {
   return (
     <div className='App'>
-      <VideoList />
+      {/* <VideoList /> */}
+      <Header />
     </div>
   );
 }
