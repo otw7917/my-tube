5cd859501b2dd449c132801387ab9498aebdfd1a
otw7917 <otw7917@gmail.com>
2022-12-09 12:07:38 +0900

    ui: header searchbar markup

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -33,16 +33,30 @@ function Header() {
   };
 
   return (
-    <div className='w-full h-20 flex items-center '>
+    <div className='w-full h-20 flex items-center mx-10'>
       <div className='text-lg font-bold flex-1'>
         <a href='/'>MYTUBE</a>
       </div>
       <div className='flex-grow'>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor='searchBar'>
-            <input type='text' id='searchBar' name='searchBar' />
+        <form
+          onSubmit={handleSubmit}
+          className='w-3/4 bg-slate-400  border-blue-600 rounded-xl flex'
+        >
+          <label htmlFor='searchBar' className='p-2 flex-grow'>
+            <input
+              type='text'
+              id='searchBar'
+              name='searchBar'
+              className='bg-slate-400 font-bold w-full outline-none caret-blue-600 px-2 focus:ring-4 focus:ring-blue-600 '
+              placeholder='원하는 영상을 찾아보세요'
+            />
           </label>
-          <button type='submit'>🔎</button>
+          <button
+            type='submit'
+            className='bg-blue-600 p-2 rounded-r-xl hover:bg-blue-800'
+          >
+            🔎
+          </button>
         </form>
       </div>
       <div className='flex-1'></div>
