9e850cfe845f327b084b5c73636e2bdabd0716f1
otw7917 <otw7917@gmail.com>
2023-01-05 11:46:57 +0900

    react router routing+ with loader func

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,35 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import VideoDetail from "./components/videoDetail";
-import Video, { getPopularVideoList } from "./pages/Video";
+import Video from "./pages/Video";
+import { MockYoutubeApi } from "./api/mockYoutube";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const mockYoutubeApi = new MockYoutubeApi();
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <Video />, loader: getPopularVideoList },
-      { path: "video", element: <Video /> },
-      { path: "video/:keyword", element: <Video /> },
+      {
+        index: true,
+        element: <Video />,
+        loader: mockYoutubeApi.getPopularVideoList,
+      },
+      {
+        path: "video",
+        element: <Video />,
+        loader: mockYoutubeApi.getPopularVideoList,
+      },
+      {
+        path: "video/:keyword",
+        element: <Video />,
+        loader: mockYoutubeApi.getSearchVideoList,
+      },
       {
         path: "video/watch/:videoId",
         element: <VideoDetail />,
